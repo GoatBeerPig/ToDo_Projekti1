6e7c37013db9ce455de987e0ed6dfbbeceeb4f4a
GoatBeerPig <hamalainentomi37@gmail.com>
2021-05-17 10:45:57 +0300

    edited

--- ToDo.js
+++ ToDo.js
@@ -1,47 +1,42 @@
 
-function insertRows(){
 
-    // Find a <table> element with id="myTable":
-    var table = document.getElementById("ToDo");
-    var btn = document.getElementById('btn');
-    var check = document.getElementById('checked');
-    var clone = btn.cloneNode(true);
-    var clone2 = check.cloneNode(true);
-
-    
-    var what = document.getElementById('textfield').value;
-    var when  = document.getElementById('textfield2').value;
-    var who = document.getElementById('textfield3').value;
-    var jono = [textfield.value, textfield2.value, textfield3.value]
-    localStorage.setItem("lista", jono);
-    
-  
-    if(what.length<=0) {
-        alert("What needs to be done is required!");
-    }
-    else if(what.length > 50 || when.length>50 ||who.length>50) {
-        alert("Too long!");
+$(document).ready(function(){
+    $("#insert_button").click(function(){
+        var value1 = $("#textfield").val();
+        var value2 = $("#textfield2").val();
+        var value3 = $("#textfield3").val();
+        if(value1.length<=0) {
+          alert("What needs to be done is required!");
+      }
+      else if(value1.length > 50 || value2.length>50 ||value3.length>50) {
+        alert("Something is too long!");
+        $("input:text").val("");
     }
     else {
-        var row = table.insertRow();
-        var cell1 = row.insertCell(0);
-         var cell2 = row.insertCell(1);
-         var cell3 = row.insertCell(2);
-         var cell4 = row.insertCell(3);
-         var cell5 = row.insertCell(4);
-        cell1.innerHTML = what;
-        cell2.innerHTML = when;
-        cell3.innerHTML = who;
-        cell4.appendChild(clone2);
-        cell5.appendChild(clone);
-
+      //lisätään uusi rivi ja laitetaan siihen arvot
+        new_row = "<tr><td>"+value1+"</td><td>"+value2+"</td><td>"+value3+"</td><td></td></tr>";
+        table = $("#ToDo");
+        table.append(new_row);
+        $("#delbtn").clone().appendTo("td:last");
+        $("input:text").val("");
     }
-    };
+      });
+});
 
-    function deleteRow(row) {
-        var i = row.parentNode.parentNode.rowIndex;
-        document.getElementById("ToDo").deleteRow(i);
-      };
+
+$(document).ready(function(){
+  $("#ToDo").on("click", "#delbtn", function(){
+    $(this).closest("tr").fadeOut();
+
+  });
+});
+
+$(document).ready(function(){
+  $("#ToDo").on("click", "tr", function(){
+    $(this).toggleClass("stroked");
+
+  });
+});
 
 
 
